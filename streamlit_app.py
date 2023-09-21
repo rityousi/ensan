165028a45f900bbb20091a7231f90209dd954f58
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-21 04:51:59 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import random
 
-st.title("分野決定ガチャ")
-if st.button("分野を決める"):
+st.title("志望校ガチャ")
+if st.button("志望校を決める"):
    results = ["代数学","解析学","幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学","位相幾何学"]
    result = random.choice(results)
-   st.write(f"結果:位相幾何学")
+   st.write(f"結果:日本文理大学")
