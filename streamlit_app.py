44471e2ef87f3d346a27a88995fb5630cea020f6
rityousi <144981077+rityousi@users.noreply.github.com>
2023-09-21 04:44:43 +0000

    分野ガチャ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import random
 
-st.title("おみくじアプリ")
-if st.button("おみくじを引く"):
+st.title("分野決定ガチャ")
+if st.button("分野を決める"):
    results = ["代数学","解析学","幾何学","幾何学","幾何学","幾何学"]
    result = random.choice(results)
    st.write(f"結果:{result}")
