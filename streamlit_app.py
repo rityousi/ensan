799dd23b16d1b8716a986d14b275b55c2cee6b33
rityousi <144981077+rityousi@users.noreply.github.com>
2024-04-17 08:52:14 +0000

    シルバーアッシュ

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ options = ["スカジ", "スペクター", "チェン"]
 selected_option = st.selectbox("キャラクターを選択してください", options)
 
 # 選択された項目に応じて対応する内容を表示
-if selected_option == "スカジ":
+if selected_option == "アンジェリーナ":
     options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
     selected_option = st.selectbox("スキルを選択してください", options)
     if selected_option == "迅速攻撃γ":
@@ -25,10 +25,53 @@ if selected_option == "スカジ":
         st.write("攻撃力、防御力、最大HP+130%")
         options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
         selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
-elif selected_option == "スペクター":
-    st.write("項目2が選択されました。ここに項目2の内容を表示します。")
-elif selected_option == "チェン":
-    st.write("項目3が選択されました。ここに項目3の内容を表示します。")
-
-
+elif selected_option == "イフリータ":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "エイヤフィヤトラ":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "エクシア":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "サリア":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "シルバーアッシュ":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "シージ":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
+elif selected_option == "ホシグマ":
+    options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
+    selected_option = st.selectbox("スキルを選択してください", options)
+    if selected_option == "迅速攻撃γ":
+        st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(上から防御力の低い順です)", options)
 
