c806683736704921d1913790add43dd0885f13b0
rityousi <144981077+rityousi@users.noreply.github.com>
2024-04-17 08:41:43 +0000

    オリジムシ１

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
 # ページのタイトルを設定
-st.title("項目選択サンプル")
+st.title("アークナイツダメージシミュレータ")
 
 # 選択可能な項目のリストを作成
 options = ["スカジ", "スペクター", "チェン"]
 
 # 選択された項目を受け取るセレクトボックスを表示
-selected_option = st.selectbox("項目を選択してください", options)
+selected_option = st.selectbox("キャラクターを選択してください", options)
 
 # 選択された項目に応じて対応する内容を表示
 if selected_option == "スカジ":
     options = ["迅速攻撃γ","波濤の裂刃","海嘯の悲歌"]
-    selected_option = st.selectbox("項目を選択してください", options)
+    selected_option = st.selectbox("スキルを選択してください", options)
     if selected_option == "迅速攻撃γ":
         st.write("攻撃力+45%,攻撃速度+45")
+        options = ["オリジムシ","兵士","機動盾隊長","軽装隊長","重装隊長","	遊撃隊盾兵隊長","「最後の蒸気騎士」"]
+        selected_option = st.selectbox("敵を選択してください(左から防御力の低い順です)", options)
     elif selected_option == "波濤の裂刃":
         st.write("配置後30秒間、攻撃力+170%")
     elif selected_option == "海嘯の悲歌":
